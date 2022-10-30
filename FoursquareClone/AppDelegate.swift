6230a69998878a1cf6a4539e8ea279d2f3df5566
serkanboyraci <serkanboyraci@gmail.com>
2022-10-30 13:11:34 +0300

    pods 'Parse' added.

--- FoursquareClone/AppDelegate.swift
+++ FoursquareClone/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import ParseSwift
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration { (ParseMutableClientConfiguration) in // to reach parse server
+            ParseMutableClientConfiguration.applicationId = ""
+            ParseMutableClientConfiguration.clientKey = ""
+            ParseMutableClientConfiguration.server = ""
+            
+        }
+        
+        Parse.initialize(with: configuration)
+        
         return true
     }
 
--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ target 'FoursquareClone' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 pod 'ParseSwift'
+pod 'Parse'
 
 
 
