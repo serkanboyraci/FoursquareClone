4a9637630ba463ea5064d8839ee8ddfc514a1c87
serkanboyraci <serkanboyraci@gmail.com>
2022-11-05 14:20:01 +0300

    detailsVC added and defined

---
+++ FoursquareClone/DetailsVC.swift
@@ -0,0 +1,31 @@
+//
+//  DetailsVC.swift
+//  FoursquareClone
+//
+//  Created by Ali serkan Boyracı  on 5.11.2022.
+//
+
+import UIKit
+import MapKit
+
+class DetailsVC: UIViewController {
+    
+    @IBOutlet var detailsImageView: UIImageView!
+    
+    @IBOutlet var detailsNameLabel: UILabel!
+    
+    @IBOutlet var detailsTypeLabel: UILabel!
+    
+    @IBOutlet var detailsDescriptionLabel: UILabel!
+    
+    @IBOutlet var detailsMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
