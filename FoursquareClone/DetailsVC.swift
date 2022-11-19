c9d3115eab270b69274f1661e9acdf5605bf4eae
serkanboyraci <serkanboyraci@gmail.com>
2022-11-20 01:19:50 +0300

    adding MapView and annotation at DetailsVC

--- FoursquareClone/DetailsVC.swift
+++ FoursquareClone/DetailsVC.swift
@@ -24,19 +24,83 @@ class DetailsVC: UIViewController {
     
     var chosenPlaceId = "" // to equaled ID to other side
     
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getDataFromParse()
+        
+        
+
+    }
+    
+    func getDataFromParse() {
         let query = PFQuery(className: "Places")
         query.whereKey("objectId", equalTo: chosenPlaceId)
         query.findObjectsInBackground { (objects, error) in
             if error != nil {
                 
             } else {
-                print(objects)
+                if objects != nil {
+                    let chosenPlaceObject = objects![0]
+                    
+                    // OBJECTS
+                    if let placeName = chosenPlaceObject.object(forKey: "name") as? String {
+                        self.detailsNameLabel.text = placeName
+                    }
+                    if let placeType = chosenPlaceObject.object(forKey: "type") as? String {
+                        self.detailsTypeLabel.text = placeType
+                    }
+                    if let placeDescription = chosenPlaceObject.object(forKey: "describe") as? String {
+                        self.detailsDescriptionLabel.text = placeDescription
+                    }
+                    if let placeLatitude = chosenPlaceObject.object(forKey: "latitude") as? String { //to take coordinates
+                        if let placeLatitudeDouble = Double(placeLatitude) {
+                            self.chosenLatitude = placeLatitudeDouble
+                        }
+                    }
+                    if let placeLongitude = chosenPlaceObject.object(forKey: "longitude") as? String {
+                        if let placeLongitudeDouble = Double(placeLongitude) {
+                            self.chosenLongitude = placeLongitudeDouble
+                        }
+                    }
+                    // to take image: first take image data and  change it to UIImageView
+                    if let imageData = chosenPlaceObject.object(forKey: "image") as? PFFileObject {
+                        imageData.getDataInBackground { (data, error) in
+                            if error == nil {
+                                if data != nil { // to prevent any error
+                                    self.detailsImageView.image = UIImage(data: data!)
+                                }
+                            }
+                        }
+                    }
+                    //MAPS
+                    
+                    let location = CLLocationCoordinate2D(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+                    
+                    let span = MKCoordinateSpan(latitudeDelta: 0.0010, longitudeDelta: 0.0010)
+                    
+                    let region = MKCoordinateRegion(center: location, span: span)
+                    
+                    self.detailsMapView.setRegion(region, animated: true)
+                    
+                    // to show exact position with annotation
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = location
+                    annotation.title = self.detailsNameLabel.text!
+                    annotation.subtitle = self.detailsTypeLabel.text!
+                    self.detailsMapView.addAnnotation(annotation)
+                    
+                        
+                    
+                    
+                    
+                }
             }
         }
-
 
     }
     
